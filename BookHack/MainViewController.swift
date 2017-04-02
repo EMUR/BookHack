d411123df0a7ed7ef5c72471ffe3a75ace6f6eae
Andy <bobo.borghi@me.com>
2017-04-01 20:38:05 -0700

    Eliminated misspelt file

--- BookHack/MainViewController.swift
+++ BookHack/MainViewController.swift
@@ -9,18 +9,13 @@
 import UIKit
 
 class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    
-    
     @IBOutlet weak var seachView: UITextField!
-    
     
     @IBOutlet weak var previewCollection: UICollectionView!
     var books = [Dictionary<String, Any>]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         seachView.layer.shadowColor = UIColor.black.cgColor
         seachView.layer.shadowOpacity = 0.4
@@ -30,23 +25,17 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         seachView.borderStyle = UITextBorderStyle.none
         
         //seachView.backgroundColor = UIColor(colorLiteralRed: Float(253), green: Float(253), blue: Float(253), alpha: 1.0)
-
-
         
         previewCollection.delegate = self
         previewCollection.dataSource = self
         
         let book = ConnectionHandler.sharedInstance
-        
     
         book.getArrayOf { (resul: Bool, array: [Dictionary<String, Any>]) in
-            for i in array
-            {
+            for i in array {
                 self.books.append(i)
             }
         }
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -57,19 +46,15 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return books.count
     }
-
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = previewCollection.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!BookCollectionViewCell
         
         cell.CoverImg.sd_setImage(with: URL(string: books[indexPath.row]["url"] as! String) , placeholderImage: UIImage(named: ""))
-        
-        
     
         return cell
     }
     @IBAction func relaod(_ sender: Any) {
-        
         previewCollection.reloadData()
         
     }
@@ -81,14 +66,4 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
