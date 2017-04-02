7bbee61508006772732b87267ede2e59cc6a7858
Andy <bobo.borghi@me.com>
2017-04-01 19:15:32 -0700

    Streamlined connection handler

--- BookHack/ConnectionHandler.swift
+++ BookHack/ConnectionHandler.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate
-{
+class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate {
     // MARK: Authentication
-    
     static let sharedInstance = ConnectionHandler(maketype: "Book")
-    
+    var table : MSTable?
+    var store : MSCoreDataStore?
+    var fetchedResultController = NSFetchedResultsController<NSFetchRequestResult>()
    
-    static var dataType:String!
+    static var dataType: String!
     
     // If using a SAS token, fill it in here.  If using Shared Key access, comment out the following line.
     var containerURL = "https://bookhackstorage.blob.core.windows.net/hellobook?st=2017-04-01T18%3A29%3A00Z&se=2017-04-02T18%3A29%3A00Z&sp=rwdl&sv=2015-12-11&sr=c&sig=cBqvsllgXYIWQVE55Ss0Aa9hDHZlBC1JQV5kl9jXYs4%3D"
@@ -24,7 +24,7 @@ class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate
     var container : AZSCloudBlobContainer?
     var continuationToken : AZSContinuationToken?
     
-    init(maketype:String) {
+    private init(maketype:String) {
         super.init()
         
         var error: NSError?
@@ -32,11 +32,25 @@ class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate
         self.continuationToken = nil
         
         ConnectionHandler.dataType = maketype
-        InitilizeConnection.sharedInstance.establishConnection()
+        ConnectionHandler.sharedInstance.establishConnection()
     }
-
     
-       
+    func establishConnection() {
+        var error : NSError? = nil
+        do {
+            try fetchedResultController.performFetch()
+        } catch let error1 as NSError {
+            error = error1
+            print("Unresolved error \(String(describing: error)), \(String(describing: error?.userInfo))")
+            abort()
+        }
+        
+        let client = MSClient(applicationURLString: "https://bookhack.azurewebsites.net")
+        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext!
+        store = MSCoreDataStore(managedObjectContext: managedObjectContext)
+        client.syncContext = MSSyncContext(delegate: nil, dataSource: store, callback: nil)
+        table = client.table(withName: ConnectionHandler.dataType)
+    }
     
     func uploadBlob() {
         let blob = container!.blockBlobReference(fromName: "Test")
@@ -62,7 +76,6 @@ class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate
         }
     }
     
-    
     func getBlobList() {
         container!.listBlobsSegmented(with: nil, prefix: nil, useFlatBlobListing: true, blobListingDetails: [], maxResults: 50) { (error : Error?, results : AZSBlobResultSegment?) -> Void in
             
@@ -78,33 +91,40 @@ class ConnectionHandler : NSObject,NSFetchedResultsControllerDelegate
             //self.tableView.performSelectorOnMainThread("reloadData", withObject: nil, waitUntilDone: false)
         }
     }
-
     
-       
-    
-    func addElement(Object:Any)
-    {
-        
+    func addElement(Object: Any) {
         if let bookObj = Object as? Book {
-            
             let itemToInsert = ["bookname": bookObj.name, "author": bookObj.author, "ISBN": bookObj.ISBN, "url": bookObj.url] as [AnyHashable : Any]
             
-            InitilizeConnection.sharedInstance.table!.insert(itemToInsert, completion: { (item, error) in
+            ConnectionHandler.sharedInstance.table!.insert(itemToInsert, completion: { (item, error) in
                 if error != nil {
                     print("Error: " + (error! as NSError).description)
                 }
             })
-            
         }
     }
     
-    func getArrayOf(completion: @escaping (_ success: Bool, _ items: [Dictionary<String,Any>]) -> Void)
-    {
+    func fetchBookByTitle() {
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: ConnectionHandler.dataType)
+        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext!
         
+        // show only non-completed items
+        fetchRequest.predicate = NSPredicate(format: "deleted != true")
+        
+        // sort by item text
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
+        
+        // Note: if storing a lot of data, you should specify a cache for the last parameter
+        // for more information, see Apple's documentation: http://go.microsoft.com/fwlink/?LinkId=524591&clcid=0x409
+        let resultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        resultsController.delegate = self
+    }
+    
+    func getArrayOf(completion: @escaping (_ success: Bool, _ items: [Dictionary<String,Any>]) -> Void) {
         var Arr = [Dictionary<String,Any>]()
         
-        
-        InitilizeConnection.sharedInstance.table?.read{ (result, error) in
+        ConnectionHandler.sharedInstance.table?.read{ (result, error) in
             if let err = error {
                 print("ERROR ", err)
             } else if let items = result?.items {
